eca7164186e8af9260c7e6ee71d772a78d22836f
Khadeeja Alif <khadeeja@fantacode.com>
2019-01-18 13:35:07 +0530

    Progress Content Changes

--- TestApp/TestApp/ViewModels/CircularProgressViewModel.cs
+++ TestApp/TestApp/ViewModels/CircularProgressViewModel.cs
@@ -9,10 +9,16 @@ namespace TestApp.ViewModels
         readonly INavigationService _navigationService;
 
         public ObservableCollection<string> Blah { get; set; }
+        public int Label1 { get; set; }
+        public int Label2 { get; set; }
+        public int Label3 { get; set; }
         public CircularProgressViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
             showList = true;
+            Label1 = 2;
+            Label2 = 4;
+            Label3 = 2;
             Blah = new ObservableCollection<string>() { "Hello", "Hi", "Bye" };
             RaisePropertyChanged("Blah");
         }
